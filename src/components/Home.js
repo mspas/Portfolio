5966b55cdcc7b40307f0bf09aa8a5c72dd873d3d
Marcin Spasinski <marcin7789@gmail.com>
2020-02-11 17:22:56 +0100

    particles as bg for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,40 +3,63 @@ import "../styles/app.sass";
 import "../styles/home.sass";
 import git from "../assets/git-ico.png";
 import lin from "../assets/lin-ico.png";
+import Particles from "react-particles-js";
+
+const particleOpt = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#C7493A"
+    },
+    line_linked: {
+      color: "#C7493A",
+      opacity: 0.3
+    }
+  }
+};
 
 function Home() {
   return (
-    <div className="home-section">
-      <div className="home-desc-divider">
-        <div className="home-desc-wrap">
-          <h3 className="home-desc">
-            Hi, I'm Marcin Spasiński - computer science{" "}
-            <span className="colorFade">student</span> living in Wrocław.{" "}
-            <span className="colorFade">Frontend</span> dev wannabe. I just want
-            to make some nice looking things. Not afraid of some{" "}
-            <span className="colorFade">backend</span> work aswell.
-          </h3>
-        </div>
-        <div className="row links">
-          <div className="col col-r col-md-6">
-            <a id="git" href="https://gihtub.com/mspas">
-              <img src={git} height={50} alt="GitHub" /> <br />
-              GitHub
-            </a>
+    <div>
+      <div className="home-section">
+        <div className="home-desc-divider">
+          <div className="home-desc-wrap">
+            <h3 className="home-desc">
+              Hi, I'm Marcin Spasiński - computer science{" "}
+              <span className="colorFade">student</span> living in Wrocław.{" "}
+              <span className="colorFade">Frontend</span> dev wannabe. I just
+              want to make some nice looking things. Not afraid of some{" "}
+              <span className="colorFade">backend</span> work aswell.
+            </h3>
           </div>
-          <div className="col col-l col-md-6">
-            <a id="lin" href="https://linkedin.com/">
-              <img src={lin} height={50} alt="Linkedin" /> <br />
-              Linkedin
-            </a>
+          <div className="row links">
+            <div className="col col-r col-md-6">
+              <a id="git" href="https://gihtub.com/mspas">
+                <img src={git} height={50} alt="GitHub" /> <br />
+                GitHub
+              </a>
+            </div>
+            <div className="col col-l col-md-6">
+              <a id="lin" href="https://linkedin.com/">
+                <img src={lin} height={50} alt="Linkedin" /> <br />
+                Linkedin
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="angle-divider-wrap home-history-divider">
+          <div className="container center" style={{ height: 500 }}>
+            <h2 className="home-history">also big history nerd...</h2>
           </div>
         </div>
       </div>
-      <div className="angle-divider-wrap home-history-divider">
-        <div className="container center" style={{ height: 500 }}>
-          <h2 className="home-history">also big history nerd...</h2>
-        </div>
-      </div>
+      <Particles className="particles-wrap" params={particleOpt} />
     </div>
   );
 }
