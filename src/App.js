ec8140af15b646712055e535d04ece9fb92c75a8
Marcin Spasinski <marcin7789@gmail.com>
2020-02-17 23:51:56 +0100

    navbar v0.5

--- src/App.js
+++ src/App.js
@@ -3,16 +3,69 @@ import "./styles/app.sass";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-      <Skills />
-      <Projects />
-      <footer>2020</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrolledPage: "about",
+      skillsOffset: 1000,
+      projectsOffset: 2000
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    let so = document.getElementById("skills").offsetTop;
+    let po = document.getElementById("projects").offsetTop;
+    this.setState({
+      skillsOffset: so,
+      projectsOffset: po
+    });
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll(event) {
+    let heightFix = window.innerHeight / 1.7;
+    if (window.pageYOffset < this.state.skillsOffset)
+      this.setActiveSection("about");
+    if (
+      window.pageYOffset > this.state.skillsOffset &&
+      window.pageYOffset < this.state.projectsOffset + heightFix
+    )
+      this.setActiveSection("skills");
+    if (window.pageYOffset > this.state.projectsOffset - heightFix)
+      this.setActiveSection("projects");
+  }
+
+  setActiveSection(name) {
+    this.setState({
+      scrolledPage: name
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header scrolledPage={this.state.scrolledPage} />
+        <div id="about">
+          <Home />
+        </div>
+        <div id="skills">
+          <Skills />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <footer>2020</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,113 @@
 import React from "react";
-import "../styles/app.sass";
 import "../styles/header.sass";
 
-function Header() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top fadeInDown">
-      <a className="navbar-brand" href="#home-section"></a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarCollapse"
-        aria-controls="navbarTogglerDemo01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0 nav-content">
-          <li className="nav-item active">
-            <a className="nav-link" href="#home-section">
-              HOME <span className="sr-only">(current)</span>
-            </a>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { scrolledPage: "about", currentActive: "about" };
+  }
+
+  componentDidUpdate() {
+    if (this.state.scrolledPage !== this.state.currentActive) {
+      let currentView = this.state.currentActive;
+      let nextView = this.state.scrolledPage;
+      this.setActive(nextView);
+      this.setInactive(currentView);
+      this.setState({
+        currentActive: nextView
+      });
+    }
+  }
+
+  setActive(id) {
+    let e = document.getElementsByClassName("nav-" + id);
+    for (let i = 0; i < e.length; i++) {
+      const element = e[i];
+      let cl = element.getAttribute("class");
+      element.setAttribute("class", cl + " active");
+    }
+  }
+
+  setInactive(id) {
+    let e = document.getElementsByClassName("nav-" + id);
+    for (let i = 0; i < e.length; i++) {
+      const element = e[i];
+      let cl = element.getAttribute("class");
+      let newCl = cl.replace("active", "");
+
+      element.setAttribute("class", newCl);
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return { scrolledPage: props.scrolledPage };
+  }
+
+  render() {
+    return (
+      <nav className="navbar-side">
+        <ul>
+          <li className="nav-link nav-home" onClick={this.onRightSlide}>
+            <svg height="20" width="100">
+              <circle
+                className="circle-outside"
+                cx="50"
+                cy="10"
+                r="6"
+                fill="white"
+              />
+              <circle
+                className="nav-about circle-inside active"
+                cx="50"
+                cy="10"
+                r="4"
+                fill="white"
+              />
+            </svg>
+            <span className="nav-about nav-label active">about</span>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#skills-section">
-              SKILLS
-            </a>
+          <li className="nav-link nav-skills">
+            <svg height="20" width="100">
+              <circle
+                className="circle-outside"
+                cx="50"
+                cy="10"
+                r="6"
+                fill="white"
+              />
+              <circle
+                className="nav-skills circle-inside"
+                cx="50"
+                cy="10"
+                r="4"
+                fill="white"
+              />
+            </svg>
+            <span className="nav-skills nav-label">skills</span>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#projects-section">
-              PROJECTS
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#projects-section">
-              CONTACT
-            </a>
+          <li className="nav-link nav-projects">
+            <svg height="20" width="100">
+              <circle
+                className="circle-outside"
+                cx="50"
+                cy="10"
+                r="6"
+                fill="white"
+              />
+              <circle
+                className="nav-projects circle-inside"
+                cx="50"
+                cy="10"
+                r="4"
+                fill="white"
+              />
+            </svg>
+            <span className="nav-projects nav-label">projects</span>
           </li>
         </ul>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 export default Header;
