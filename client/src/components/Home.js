e66f4206abc82c8649af1c0db4a55a5c60b24a70
Marcin Spasinski <marcin7789@gmail.com>
2020-07-04 16:48:53 +0200

    bug fix, changed update rate for bg move

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHandPointDown } from "@fortawesome/free-regular-svg-icons";
 
 const mouse = new Mouse();
-const updateRate = 1;
+const updateRate = 3;
 
 class Home extends React.Component {
   constructor(props) {
